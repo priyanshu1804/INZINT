71f0877858b3aa5eb97af6b2744f2dbcc64cf361
priyanshu1804 <priyanshu.thakur024@gmail.com>
2025-10-02 00:34:59 +0530

    eight commit

--- Backend/index.js
+++ Backend/index.js
@@ -6,15 +6,17 @@ const userRoutes=require("./routes/user");
 const taskRoutes=require("./routes/task");
 const app=express();
 const db=require('./db');
-const corsOptions = {
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"], 
-  credentials: true,
-};
+// const corsOptions = {
+//   origin: "*",
+//   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//   allowedHeaders: ["Content-Type", "Authorization"], 
+//   credentials: true,
+// };
 
-app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+// app.use(cors(corsOptions));
+// app.options("*", cors(corsOptions));
+app.use(cors());
+db();
 app.use(express.json());
 app.use("/user",userRoutes);
 app.use("/task",taskRoutes);
