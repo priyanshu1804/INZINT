c7b55024ce7b3d2c762c5b2a9671c43c49680972
priyanshu1804 <priyanshu.thakur024@gmail.com>
2025-10-01 16:25:49 +0530

    eight commit

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API=axios.create({baseURL:"https://inzint-stjp-ebon.vercel.app/"});
+const API=axios.create({baseURL:"https://inzint-1.onrender.com/"});
 API.interceptors.request.use((req)=>{
     const token=localStorage.getItem("token");
     if(token){
