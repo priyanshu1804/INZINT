5b52f89a602f4dc8f36bc481f87d9711f061e66f
priyanshu1804 <priyanshu.thakur024@gmail.com>
2025-10-02 00:27:02 +0530

    eight commit

--- Backend/index.js
+++ Backend/index.js
@@ -6,19 +6,15 @@ const userRoutes=require("./routes/user");
 const taskRoutes=require("./routes/task");
 const app=express();
 const db=require('./db');
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://inzint.vercel.app");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", "true");
-  
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200); 
-  }
-  
-  next();
-});
+const corsOptions = {
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"], 
+  credentials: true,
+};
 
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 app.use(express.json());
 app.use("/user",userRoutes);
 app.use("/task",taskRoutes);
